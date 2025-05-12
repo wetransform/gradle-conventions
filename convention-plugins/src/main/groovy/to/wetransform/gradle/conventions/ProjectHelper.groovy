cb06836c9c92fdc96e6f3953acf0822241543857
Simon Templer <simon@wetransform.to>
2025-05-12 15:29:09 +0200

    feat: better detection if a project uses Java, Groovy or Kotlin

---
+++ convention-plugins/src/main/groovy/to/wetransform/gradle/conventions/ProjectHelper.groovy
@@ -0,0 +1,25 @@
+package to.wetransform.gradle.conventions
+
+import groovy.transform.CompileStatic
+import org.gradle.api.Project
+
+@CompileStatic
+class ProjectHelper {
+
+  static boolean hasJava(Project project) {
+    return project.plugins.hasPlugin('java') || project.plugins.hasPlugin('java-libary') || hasFolder(project, 'src/main/java') || hasFolder(project, 'src/test/java')
+  }
+
+  static boolean hasKotlin(Project project) {
+    return project.plugins.hasPlugin('org.jetbrains.kotlin.jvm') || hasFolder(project, 'src/main/kotlin') || hasFolder(project, 'src/test/kotlin')
+  }
+
+  static boolean hasGroovy(Project project) {
+    return project.plugins.hasPlugin('groovy') || hasFolder(project, 'src/main/groovy') || hasFolder(project, 'src/test/groovy')
+  }
+
+  static boolean hasFolder(Project project, String folder) {
+    return project.file(folder).exists()
+  }
+
+}
