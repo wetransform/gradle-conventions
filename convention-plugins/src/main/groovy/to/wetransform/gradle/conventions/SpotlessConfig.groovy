d119f4cbc94267d7bb31d30b1e75df5a465fd315
Simon Templer <simon@wetransform.to>
2025-05-15 09:51:40 +0200

    feat: add Apache pre-defined license header

--- convention-plugins/src/main/groovy/to/wetransform/gradle/conventions/SpotlessConfig.groovy
+++ convention-plugins/src/main/groovy/to/wetransform/gradle/conventions/SpotlessConfig.groovy
@@ -4,8 +4,11 @@
  */
 package to.wetransform.gradle.conventions
 
+import groovy.transform.CompileStatic
+
 import org.gradle.api.provider.Property
 
+@CompileStatic
 interface SpotlessConfig {
   /**
    * The default license header.
@@ -16,6 +19,24 @@ interface SpotlessConfig {
  * All rights reserved.
  */
 '''
+
+  static String APACHE = '''\
+    /*
+     * Copyright $YEAR wetransform GmbH
+     *
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
+    '''.stripIndent()
 
   /**
    * @return <code>true</code> if spotless should be disabled
