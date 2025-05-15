968d2a14d71542bdf729da55189ac08640944aad
Simon Templer <simon@wetransform.to>
2025-05-15 09:51:40 +0200

    feat: add Apache pre-defined license header

--- convention-plugins/src/test/groovy/to/wetransform/gradle/conventions/JavaSpotlessPluginTest.groovy
+++ convention-plugins/src/test/groovy/to/wetransform/gradle/conventions/JavaSpotlessPluginTest.groovy
@@ -112,4 +112,65 @@ class JavaSpotlessPluginTest extends PluginTest {
     then:
     result.task(":spotlessJavaCheck").outcome == TaskOutcome.SUCCESS
   }
+
+  def "succeeds for correct Java formatting with custom header"() {
+    setup:
+    buildFile.text = """
+    plugins {
+      id 'java'
+      id 'to.wetransform.conventions'
+    }
+
+    repositories {
+      mavenCentral()
+    }
+
+    wetransform {
+      setup {
+        spotless {
+          licenseHeader = APACHE
+        }
+      }
+    }
+    """.stripIndent()
+
+    and:
+    new File(testProjectDir, '.editorconfig') << """
+    root = true
+
+    [*.java]
+    indent_style = space
+    indent_size = 4
+    """.stripIndent()
+
+    new File(testProjectDir, 'src/main/java').mkdirs()
+    new File(testProjectDir, 'src/main/java/Test.java') << """\
+    /*
+     * Copyright 2025 wetransform GmbH
+     *
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
+    public class Test {
+        public static void main(String[] args) {
+            System.out.println("Indentation correct");
+        }
+    }
+    """.stripIndent()
+
+    when:
+    def result = runTask('spotlessCheck')
+
+    then:
+    result.task(":spotlessJavaCheck").outcome == TaskOutcome.SUCCESS
+  }
 }
